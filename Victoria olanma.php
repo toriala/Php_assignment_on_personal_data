5441f71188db245b5712099ea02560adf8c68f6f
toriala <133252671+toriala@users.noreply.github.com>
2023-05-28 23:20:15 +0100

    Add files via upload

---
+++ Victoria olanma.php
@@ -0,0 +1,11 @@
+<?php
+$name = "<h1>Agwu Victoria Olanma<h1><br>";
+$reg_no = "2021/hnd/33082/cs";
+$state_of_origin = "Ebonyi state";
+$project_topic = "REAL –TIME GENDER DETECTION USING CONVOLUTIONAL NEURAL NETWORK (CNN)";
+
+echo $name;
+echo $reg_no;
+echo $state_of_origin;
+echo $project_topic;
+?>
